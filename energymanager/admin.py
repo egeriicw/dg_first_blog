da4fabfc242f830c7fed175cf97a55d585c8a6d5
Bill Eger <egeriicw@gmail.com>
2017-04-25 10:37:32 -0400

    added Place and Address models to EnergyManager app

--- energymanager/admin.py
+++ energymanager/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Address, Place
+
 # Register your models here.
+
+admin.site.register(Address)
+admin.site.register(Place)
--- energymanager/models.py
+++ energymanager/models.py
@@ -1,3 +1,24 @@
+from __future__ import unicode_literals
 from django.db import models
 
+from datetime import datetime
+
 # Create your models here.
+
+class Address(models.Model):
+    street_num = models.TextField(max_length=10)
+    street_name1 = models.TextField(max_length=255)
+    street_name2 = models.TextField(max_length=255, blank=True)
+    street_name3 = models.TextField(max_length=255, blank=True)
+
+    def __str__(self):
+        return "{} {} {} {}".format(self.street_num, self.street_name1, self.street_name2, self.street_name3)
+
+class Place(models.Model):
+    name = models.TextField(max_length=255)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, blank=True)
+    date_begin = models.DateField(default=datetime.strptime("1900-01-01", "%Y-%m-%d"), blank=False, null=True)
+    date_end = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return "{}".format(self.name)
